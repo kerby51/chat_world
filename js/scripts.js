16b2768b51c273a1f5f9198c0abb6cba3882b154
katharine benefico <kbenefico@gmail.com>
2017-01-30 15:55:05 -0500

    update index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var button = document.querySelector('#button');
 var target = document.querySelector('#main');
+var form = document.getElementById('form');
+var iframe;
 var counter = 0;
 
 function createiFrame() {
@@ -14,16 +16,38 @@ function createiFrame() {
   $( function() {
     $( '.ui-widget-content' ).draggable();
   }) ;
-  console.log('working');
+  chatBox.contentWindow.addEventListener('message', showMessage);
+
+  iframe = document.querySelectorAll('iframe');
+  var form = chatBox.document.getElementById("form");
+  form.addEventListener('submit', sendMessage, false);
 }
 
-function sendMessage() {
-  var iFrame = document.getElementById('iframe0');
-  console.log(iFrame);
+function sendMessage(event) {
+  event.preventDefault();
   var message = document.querySelector('#message').value;
-  console.log(message);
-   // console.log(contentWindow);
-  iFrame.contentWindow.postMessage(message, 'other.html');
+  // var iframe = iframe.document.getElementById("postMessageButton");
+  iframe.parent.postMessage(message, "*");
+  return false;
 }
 
-button.addEventListener('click', createiFrame);
+function receiveMessage(event) {
+  var data = event.data;
+  showMessage(data);
+}
+
+function showMessage(messageTxt) {
+  var messageList = document.getElementById('list');
+  var listItem = document.createElement('li');
+  listItem.appendChild(document.createTextNode(messageTxt));
+  messageList.appendChild(listItem);
+}
+
+if (button) {
+  button.addEventListener('click', createiFrame);
+}
+
+
+
+
+
