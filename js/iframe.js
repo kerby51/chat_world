9f665cd09194fcbed7ae81b6e5ea9cbb219e8126
katharine benefico <kbenefico@gmail.com>
2017-01-31 14:51:07 -0500

    add comments

--- js/iframe.js
+++ js/iframe.js
@@ -1,19 +1,31 @@
 var parent = window.parent;
 
-var form = document.getElementById('form');
-console.log(form);
+var form = document.querySelector('.form');
 
+
+//message's innerHTML will be an li appended (and formatted by formatMessage) to the ul #list
 window.addEventListener('message', function(event) {
   var list = document.getElementById('list');
-  console.log(list);
   var newMsg = document.createElement('li');
-  newMsg.appendChild(document.createTextNode(event.data.text));
+  newMsg.innerHTML += formatMessage(event);
   list.appendChild(newMsg);
 });
 
+//form using postMessage to send message to the parent
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   var msg = document.getElementById('message').value;
-  parent.postMessage({text: msg, id: 1}, '*');
+  parent.postMessage(msg, '*');
+  form.reset();
   return false;
-})
+});
+
+//formatting the message to include a current date/time stamp
+function formatMessage(event) {
+  var string = new Date().toString();
+  var date = string.substring(4, 15).replace(/ /g, '-');
+  var time = string.substring(15, 21);
+  return date + ': ' + time + ' - ' + event.data;
+}
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
-var button = document.querySelector('#button');
-var target = document.querySelector('#main');
-var form = document.getElementById('form');
-var iframe;
-var counter = 0;
+var button = document.getElementById('button');
+var target = document.getElementById('main');
+var counter = 1;
 
+//ability to create a draggable iframe with a class, unique id, & src of 'other.html', called on button click
 function createiFrame() {
   var chatBox = document.createElement('iframe');
   var chatNum = counter;
   counter++;
   chatBox.setAttribute('class', 'ui-widget-content');
-  chatBox.setAttribute('id', 'iframe' + chatNum);
+  chatBox.setAttribute('id', 'IFrame ' + chatNum);
   chatBox.setAttribute('draggable', 'true');
   chatBox.setAttribute('src', 'other.html');
   target.appendChild(chatBox);
@@ -18,10 +17,12 @@ function createiFrame() {
   }) ;
 }
 
+//when button "+" is clicked, createiFrame function is called
 if (button) {
   button.addEventListener('click', createiFrame);
 }
 
+//parent using postMessage to send message to all iframes
 window.addEventListener('message', function(event) {
   var iframeNodes = document.querySelectorAll('iframe');
   iframeNodes.forEach(function(node) {
